a8574c7bc13b58c5859ef34d7066a43df46e4337
ematos <mme.matos.elise@gmail.com>
2022-01-02 18:27:22 +0100

    ADD list-new.component.ts : add empty list and navigate to library of todo lists

--- src/app/list-new/list-new.component.ts
+++ src/app/list-new/list-new.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ListService} from "../shared/services/list.service";
+import {List} from "../shared/models/list.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-list-new',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-new.component.css']
 })
 export class ListNewComponent implements OnInit {
+  addListForm: FormGroup;
+  title = new FormControl('', Validators.required)
+  lists: List[]=[];
+  newList: List;
 
-  constructor() { }
+  constructor(private listService: ListService, readonly router: Router) {
+    this.addListForm = new  FormGroup({
+      title: this.title
+    })
+    this.newList = {
+      title: '',
+      items: []
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  createList() {
+    this.newList.title = this.addListForm.get('title')?.value;
+    this.listService
+      .addList(this.newList)
+      .subscribe(list => {
+          this.lists.push(list);
+          this.router.navigateByUrl('/');
+        }
+      );
   }
 
 }
